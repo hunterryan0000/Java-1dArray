06d7158d23e27dd057a94ffd5775a894ca9007d1
Ryan Jamal Hunter <hunterryan0000@yahoo.com>
2022-07-10 19:03:30 -0500

    Vanguard problems should be on github

--- Workspace/SkillstormTutorials/src/com/example/Increment.java
+++
@@ -1,7 +0,0 @@
-package com.example;
-
-public class Increment {
-    public static void main(String[] args) {
-
-    }
-}
--- Workspace/SkillstormTutorials/src/com/example/hello/Frame.java
+++
@@ -1,6 +0,0 @@
-package com.example.hello;
-
-public class Frame {
-    Door door; // No need for import since Frame is in same package as Door
-    String string; //String is in java.lang and will never have to import
-}
